52cb3b537f4b2e27e404579227f184252d714683
Shahzoda <shahzodanigmatjanova524@gmail.com>
2022-11-10 16:09:55 +0600

    Quizz app finish

--- flutter_project/lib/homePage.dart
+++ flutter_project/lib/homePage.dart
@@ -3,8 +3,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_project/vaprosy.dart';
 
-
-
 class HomePage extends StatefulWidget {
   const HomePage({Key key}) : super(key: key);
 
@@ -13,22 +11,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  
   bool isPressed = false;
-  Color trueAnswer= Colors.green;
-  Color falseAnswer=Colors.red;
-  Color btnColor=Color(0xFF117eeb);
-  int number= 0;
-  void test() {
-    setState(() {});
-  }
-  
-
+  Color trueAnswer = Colors.green;
+  Color falseAnswer = Colors.red;
+  Color btnColor = Color(0xFF117eeb);
+  PageController _controller = PageController(initialPage: 0);
+  int number = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      
       appBar: AppBar(
         title: Center(
           child: Text(
@@ -37,73 +29,102 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       body: PageView.builder(
-        
-        itemCount: questions.length,
-        itemBuilder: (context, index){
-        return Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, 
-          children: [SizedBox(
-            height: 30,
-            width: double.infinity,
-           child: Center(
-             child: Text(questions  [index].question ,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20.0,
-              ),
-              ),
-           ),
-          ),
-              SizedBox(
-                height: 50,
-              ),
-              for( int i = 0; i< questions[index].answers.length;i ++)
-              Container(
-                width: double.infinity,
-                margin: EdgeInsets.only(bottom: 18.0,),
-                height: 30,
-                color: isPressed?questions[index].answers.entries.toList()[i].value?
-                trueAnswer:falseAnswer:btnColor,
-                child: TextButton(
-                  onPressed: (() {
-                    setState(() {
-                        isPressed=true;
-                      });
-       if(questions[index].answers.entries.toList()[i].value)
-         { } else{
-                   setState(() {
-                    btnColor=falseAnswer;
-                  });
-                 }
-                   }), 
-                child: Text(questions[index].answers.keys.toList()[i],
-                style: TextStyle(color: Colors.black,
-                fontSize: 20,
-                ),
-                ),
-                  ),
-                    ),
-                     SizedBox(
-                height: 30,
-              ),
-              Row(
-                children: [
-                  TextButton(onPressed: (() {
-                    
-                  }),
-                   child: Text("Next Question",
-                   style: TextStyle(color: Colors.white),
-                   )
-                   )
-                ],
-              )
-             
-              ], ),
-        );
+        controller: _controller,
+        onPageChanged: (Page) {
+          setState(() {
+            isPressed = false;
+          });
         },
-      )
-   );
-  }
-  
-  }
+        itemCount: questions.length,
+        itemBuilder: (context, index) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 30,
+                  width: double.infinity,
+                  child: Center(
+                    child: Text(
+                      questions[index].question,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                for (int i = 0; i < questions[index].answers.length; i++)
+                  Container(
+                    width: double.infinity,
+                    margin: EdgeInsets.only(
+                      bottom: 18.0,
+                    ),
+                    height: 40,
+                    color: isPressed
+                        ? questions[index].answers.entries.toList()[i].value
+                            ? trueAnswer
+                            : falseAnswer
+                        : btnColor,
+                    child: TextButton(
+                      onPressed: (() {
+                        setState(() {
+                          isPressed = true;
+                        });
+                        if (questions[index]
+                            .answers
+                            .entries
+                            .toList()[i]
+                            .value) {
+                        } else {
+                          setState(() {
+                            btnColor = btnColor;
+                          });
+                        }
+                      }),
+                      child: Text(
+                        questions[index].answers.keys.toList()[i],
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 30,
+                        ),
+                      ),
+                    ),
+                  ),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    OutlinedButton(
+                      onPressed: isPressed
+                          ? () {
+                              _controller.nextPage(
+                                  duration: Duration(milliseconds: 550),
+                                  curve: Curves.linear);
+                            }
+                          : null,
 
+                      //  style: ButtonStyle(),
+
+                      child: Text(
+                        "Next Question",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
--- flutter_project/lib/question.dart
+++ flutter_project/lib/question.dart
@@ -1,6 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
 class QuestionModel{
   String question;
-  Map<String,bool> answers;
+  Map<String,bool>answers;
 
   QuestionModel(this.question, this.answers);
 }
+class Tovarlar{
+ final String name ;
+  final int price;
+  final String description;
+
+Tovarlar ({
+  this.description,
+  this.name,
+  this.price,
+  });
+  
+  static  List<Tovarlar> tovar =[
+  Tovarlar(
+    description: "red",
+    name: "Iphone",
+    price: 100,
+    
+  ),
+   
+];
+ 
+}
+  
